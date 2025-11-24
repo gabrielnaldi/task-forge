f7b23a4d99dd722ee693d4e94400923d0740ddf4
Gabriel N <gabrielnaldi.castro@gmail.com>
2025-11-24 19:50:15 -0300

    test: ensure task title can receive values

--- src/api/__tests__/domain/value-objects/task-title.value-object.spec.ts
+++ src/api/__tests__/domain/value-objects/task-title.value-object.spec.ts
@@ -2,14 +2,18 @@ import { TaskTitle } from '@src/api/domain/value-objects/task-title.value-object
 
 describe('TaskTitle value-object tests', () => {
   it('should make a new TaskTitle', () => {
-    const task_title = new TaskTitle();
+    const value = 'Make new item';
+    const task_title = new TaskTitle(value);
 
     expect(task_title).toBeDefined();
+    expect(task_title.value).toBe(value);
   });
 
   it('should create a TaskTitle through factory', () => {
-    const task_title = TaskTitle.create();
+    const value = 'Make new item';
+    const task_title = TaskTitle.create(value);
 
     expect(task_title).toBeDefined();
+    expect(task_title.value).toBe(value);
   });
 });
