c74733897aed0178cd0b9d9d8bfbf36cc6254584
Gabriel N <gabrielnaldi.castro@gmail.com>
2025-11-26 09:19:57 -0300

    refactor: return type on delete

--- src/api/domain/repositories/tasks.repository.ts
+++ src/api/domain/repositories/tasks.repository.ts
@@ -2,7 +2,7 @@ import { Task } from '../entities/task.entity';
 
 export abstract class TasksRepository {
   abstract save(task: Task): Promise<Task>;
-  abstract delete(taskId: string): Promise<null>;
+  abstract delete(taskId: string): Promise<undefined>;
   abstract findById(taskId: string): Promise<Task | null>;
   abstract findAll(): Promise<Task[]>;
 }
