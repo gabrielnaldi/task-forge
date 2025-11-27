9d0f7e7d99cb7ded2457dca663b627342d53acb5
Gabriel N <gabrielnaldi.castro@gmail.com>
2025-11-27 10:58:13 -0300

    feat: create use case to change a task status

---
+++ src/api/application/use-cases/update-task-status/update-task-status.use-case.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { TaskStatus } from '@src/api/domain/contracts/task.contracts';
+import { TasksRepository } from '@src/api/domain/repositories/tasks.repository';
+
+@Injectable()
+export class UpdateTaskStatusUseCase {
+  constructor(private readonly tasksRepository: TasksRepository) {}
+
+  public async execute(input: { taskId: string; newStatus: TaskStatus }) {
+    const task = await this.tasksRepository.findById(input.taskId);
+
+    if (!task) throw new Error('Task not found!');
+
+    task.changeStatus(input.newStatus);
+
+    const updated_task = await this.tasksRepository.save(task);
+
+    return updated_task;
+  }
+}
