35ec4e8ccd15242d0aff58ebb7a5c9b06cff2758
Gabriel N <gabrielnaldi.castro@gmail.com>
2025-11-27 09:44:16 -0300

    test: ensure it can create tasks

---
+++ src/__tests__/application/use-cases/create-task.use-case.spec.ts
@@ -0,0 +1,44 @@
+import { CreateTaskUseCase } from '@src/api/application/use-cases/create-task.use-case';
+import { Test } from '@nestjs/testing';
+import { Task } from '@src/api/domain/entities/task.entity';
+import { IDGenerator } from '@src/api/application/service/id-generator.service';
+import { UUIDGenerator } from '@src/api/infra/services/uuid-generator.service';
+import { TasksRepository } from '@src/api/domain/repositories/tasks.repository';
+import { InMemoryTasksRepositories } from '@src/api/application/repositories/in-memory/in-memory-tasks-repository';
+
+describe('CreateTaskUseCase tests', () => {
+  let useCase: CreateTaskUseCase;
+  let tasksRepository: TasksRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        CreateTaskUseCase,
+        {
+          provide: IDGenerator,
+          useClass: UUIDGenerator,
+        },
+        {
+          provide: TasksRepository,
+          useClass: InMemoryTasksRepositories,
+        },
+      ],
+    }).compile();
+
+    useCase = module.get<CreateTaskUseCase>(CreateTaskUseCase);
+    tasksRepository = module.get<InMemoryTasksRepositories>(TasksRepository);
+  });
+
+  it('should create a task', async () => {
+    const title = 'Task 1';
+    const description = 'First task';
+    const save_result = await useCase.execute({ title, description });
+    const search_result = await tasksRepository.findById(save_result.id);
+
+    expect(save_result).toBeDefined();
+    expect(save_result).toBeInstanceOf(Task);
+    expect(save_result.title).toBe(title);
+    expect(save_result.description).toBe(description);
+    expect(search_result).toEqual(save_result);
+  });
+});
