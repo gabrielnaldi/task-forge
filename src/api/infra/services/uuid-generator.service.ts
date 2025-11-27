619f98cffd9917047cb1679d64f49c48460cc2c8
Gabriel N <gabrielnaldi.castro@gmail.com>
2025-11-27 09:41:18 -0300

    fix: add missing decorator

--- src/api/infra/services/uuid-generator.service.ts
+++ src/api/infra/services/uuid-generator.service.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@nestjs/common';
 import { IDGenerator } from '@src/api/application/service/id-generator.service';
 import { v4 as uuidv4 } from 'uuid';
 
+@Injectable()
 export class UUIDGenerator implements IDGenerator {
   generate(): string {
     const uuid = uuidv4();
