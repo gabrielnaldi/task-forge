332f065cf7a59c2b42b31c25e37f9fa14b129c5f
Gabriel N <gabrielnaldi.castro@gmail.com>
2025-11-27 09:57:55 -0300

    refactor: input type on use case

--- src/api/application/use-cases/create-task/create-task.use-case.ts
+++ src/api/application/use-cases/create-task/create-task.use-case.ts
@@ -2,8 +2,9 @@ import { Task } from '@src/api/domain/entities/task.entity';
 import { TasksRepository } from '@src/api/domain/repositories/tasks.repository';
 import { TaskDescription } from '@src/api/domain/value-objects/task-description.value-object';
 import { TaskTitle } from '@src/api/domain/value-objects/task-title.value-object';
-import { IDGenerator } from '../service/id-generator.service';
 import { Injectable } from '@nestjs/common';
+import { IDGenerator } from '../../service/id-generator.service';
+import { CreateTaskInput } from './create-task.input';
 
 @Injectable()
 export class CreateTaskUseCase {
@@ -12,7 +13,7 @@ export class CreateTaskUseCase {
     private readonly tasksRepository: TasksRepository,
   ) {}
 
-  public async execute(input: { title: string; description: string }) {
+  public async execute(input: CreateTaskInput) {
     const task_id = this.idGenerator.generate();
     const task_title = TaskTitle.create(input.title);
     const task_description = TaskDescription.create(input.description);
