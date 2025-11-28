1ff503ce3de26fb8d0eadf0139a6f172f8668fee
Gabriel N <gabrielnaldi.castro@gmail.com>
2025-11-28 09:13:43 -0300

    feat: add use case to delete tasks

---
+++ src/api/application/use-cases/delete-task/delete-task.use-case.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { TasksRepository } from '@src/api/domain/repositories/tasks.repository';
+
+interface DeleteTaskInput {
+  taskId: string;
+}
+
+@Injectable()
+export class DeleteTaskUseCase {
+  constructor(private readonly tasksRepository: TasksRepository) {}
+
+  public async execute(input: DeleteTaskInput) {
+    const task_found = await this.tasksRepository.findById(input.taskId);
+
+    if (!task_found) throw new Error('Task not found!');
+
+    await this.tasksRepository.delete(input.taskId);
+  }
+}
