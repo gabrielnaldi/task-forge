ee97faecbf9f0b611747e8a382d7772ac469a6d9
Gabriel N <gabrielnaldi.castro@gmail.com>
2025-12-03 09:03:58 -0300

    feat: add helper method on repository just for tests

--- src/__tests__/doubles/in-memory/services/in-memory-tasks-queue.service.ts
+++ src/__tests__/doubles/in-memory/services/in-memory-tasks-queue.service.ts
@@ -2,9 +2,13 @@ import { TaskPublishedEvent } from '@src/api/application/events/tasks/task-publi
 import { TasksQueueService } from '@src/api/application/queues/tasks-queue.service';
 
 export class InMemoryTasksQueueService implements TasksQueueService {
-  events: TaskPublishedEvent[] = [];
+  private readonly events: TaskPublishedEvent[] = [];
 
   async publish(event: TaskPublishedEvent): Promise<void> {
     this.events.push(event);
+  }
+
+  public async getPublishedEvents() {
+    return this.events;
   }
 }
