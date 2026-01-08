4acfc49d549b55728d2c027a242c95a8d3d4761e
Gabriel N <gabrielnaldi.castro@gmail.com>
2026-01-08 14:49:30 -0300

    feat: add option to override data on task factory

--- src/__tests__/factories/task.factory.ts
+++ src/__tests__/factories/task.factory.ts
@@ -2,18 +2,21 @@ import { TaskContract } from '@src/domain/contracts/task.contract';
 import { Task } from '@src/domain/entities/task.entity';
 import { Title } from '@src/domain/value-objects/title.value-object';
 
+type OverrideData = Partial<TaskContract>;
+
 export class TaskFactory {
-  private static makeTaskData(): TaskContract {
+  private static makeTaskData(overrideData: OverrideData): TaskContract {
     const data: TaskContract = {
       title: Title.create('Task title - example'),
       description: 'Task description - example',
+      ...overrideData,
     };
 
     return data;
   }
 
-  public static makeTask() {
-    const data = this.makeTaskData();
+  public static makeTask(overrideData?: OverrideData) {
+    const data = this.makeTaskData(overrideData);
 
     const task = Task.create(data);
 
