c5252cb9c8fe488cd7b5969a55177f0011f997ef
Gabriel N <gabrielnaldi.castro@gmail.com>
2026-01-08 14:52:18 -0300

    refactor: adjust mocks

--- src/__tests__/domain/entities/task.entity.spec.ts
+++ src/__tests__/domain/entities/task.entity.spec.ts
@@ -3,16 +3,16 @@ import { Task } from '@src/domain/entities/task.entity';
 import { Title } from '@src/domain/value-objects/title.value-object';
 
 describe('Task - Entity', () => {
-  const TITLE_EXAMPLE = 'Task title - example';
-  const DESCRIPTION_EXAMPLE = 'Task description - example';
+  const VALID_TITLE = Title.create('Task title - example');
+  const VALID_DESCRIPTION = 'Task description - example';
 
   it('should create a task', () => {
-    const title = Title.create(TITLE_EXAMPLE);
+    const input = {
+      title: VALID_TITLE,
+      description: VALID_DESCRIPTION,
+    };
 
-    const task = Task.create({
-      title: title,
-      description: DESCRIPTION_EXAMPLE,
-    });
+    const task = Task.create(input);
 
     expect(task).toBeInstanceOf(Task);
   });
@@ -20,7 +20,7 @@ describe('Task - Entity', () => {
   it('should make sure that a task has a title', () => {
     const title = Title.create('Task title example');
 
-    const input: TaskContract = { title, description: DESCRIPTION_EXAMPLE };
+    const input: TaskContract = { title, description: VALID_DESCRIPTION };
 
     const task = Task.create(input);
 
@@ -29,11 +29,9 @@ describe('Task - Entity', () => {
   });
 
   it('should make sure that a task has a description', () => {
-    const title = Title.create(TITLE_EXAMPLE);
-
     const description = 'Task description example';
 
-    const input = { title, description };
+    const input = { title: VALID_TITLE, description };
 
     const task = Task.create(input);
 
