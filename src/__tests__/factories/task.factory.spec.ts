430c6f2ccdbdd4a6bd518c320e53d4b64d60be44
Gabriel N <gabrielnaldi.castro@gmail.com>
2026-01-08 14:49:42 -0300

    test: ensure it can override task data

--- src/__tests__/factories/task.factory.spec.ts
+++ src/__tests__/factories/task.factory.spec.ts
@@ -1,5 +1,6 @@
 import { Task } from '@src/domain/entities/task.entity';
 import { TaskFactory } from './task.factory';
+import { Title } from '@src/domain/value-objects/title.value-object';
 
 describe('Task - Factory', () => {
   it('should create a valid task', () => {
@@ -8,5 +9,21 @@ describe('Task - Factory', () => {
     expect(task).toBeInstanceOf(Task);
     expect(task.title).toBe('Task title - example');
     expect(task.description).toBe('Task description - example');
+  });
+
+  it('should override task data', () => {
+    const override_title = 'Override title';
+
+    const override_description = 'Override description';
+
+    const override_data = {
+      title: Title.create(override_title),
+      description: override_description,
+    };
+
+    const task = TaskFactory.makeTask(override_data);
+
+    expect(task.description).toBe(override_description);
+    expect(task.title).toBe(override_title);
   });
 });
