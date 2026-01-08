c7b77a23c1b5c4d2bc20da02601a842bf4eebd09
Gabriel N <gabrielnaldi.castro@gmail.com>
2026-01-08 10:53:54 -0300

    test: ensure task has description

--- src/__tests__/domain/entities/task.entity.spec.ts
+++ src/__tests__/domain/entities/task.entity.spec.ts
@@ -1,9 +1,14 @@
+import { TaskContract } from '@src/domain/contracts/task.contract';
 import { Task } from '@src/domain/entities/task.entity';
 
 describe('Task - Entity', () => {
+  const TITLE_EXAMPLE = 'Task title - example';
+  const DESCRIPTION_EXAMPLE = 'Task description - example';
+
   it('should create a task', () => {
     const task = Task.create({
       title: 'Task title example',
+      description: DESCRIPTION_EXAMPLE,
     });
 
     expect(task).toBeInstanceOf(Task);
@@ -12,11 +17,21 @@ describe('Task - Entity', () => {
   it('should make sure that a task has a title', () => {
     const title = 'Task title example';
 
-    const input = { title };
+    const input: TaskContract = { title, description: DESCRIPTION_EXAMPLE };
 
     const task = Task.create(input);
 
     expect(task).toBeInstanceOf(Task);
     expect(task.title).toBe(title);
+  });
+
+  it('should make sure that a task has a description', () => {
+    const description = 'Task description example';
+
+    const input = { title: TITLE_EXAMPLE, description };
+
+    const task = Task.create(input);
+
+    expect(task.description).toBe(description);
   });
 });
