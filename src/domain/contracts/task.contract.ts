1a9707611c3039f426302116519b9f6a048ff13d
Gabriel N <gabrielnaldi.castro@gmail.com>
2026-01-08 10:45:50 -0300

    feat: create task entity

---
+++ src/domain/contracts/task.contract.ts
@@ -0,0 +1,3 @@
+export interface TaskContract {
+  title: string;
+}
---
+++ src/domain/entities/task.entity.ts
@@ -0,0 +1,18 @@
+import { TaskContract } from '../contracts/task.contract';
+
+export class Task {
+  private readonly props: TaskContract;
+
+  private constructor(props: TaskContract) {
+    this.props = props;
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  static create(props: TaskContract) {
+    const task = new Task(props);
+    return task;
+  }
+}
