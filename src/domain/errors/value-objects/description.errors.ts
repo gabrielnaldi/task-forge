3ffa4a30dd34b96f8536af6f3842c336bc9430e6
Gabriel N <gabrielnaldi.castro@gmail.com>
2026-01-08 15:44:37 -0300

    feat: add description errors file

---
+++ src/domain/errors/value-objects/description.errors.ts
@@ -0,0 +1,25 @@
+export class DescriptionError extends Error {
+  private readonly _code: string;
+
+  constructor(message: string, code: string) {
+    super(message);
+
+    this.name = 'DescriptionError';
+    this._code = code;
+
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  public static maxLength() {
+    const error = new DescriptionError(
+      'Description must not exceed 100 characters!',
+      'MAX_LENGTH_ERROR',
+    );
+
+    return error;
+  }
+}
